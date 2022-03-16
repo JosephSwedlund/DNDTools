1c3d6b2fb9a9b67f84b40ffbd77813bd4904b19f
JosephSwedlund <josephswedlund@TypeFaster.com>
2022-03-16 00:59:12 -0500

    added source_code file and start of character creator

---
+++ source_code/character_generator/character_creator.lisp
@@ -0,0 +1,14 @@
+;;;; Character Creator using my DND stat system
+
+;;; Get Input for number of characters to create and any filter wanted
+
+
+(defvar *name* ())
+
+(defparameter *age* 18)
+
+(loop for x from 1 to 100
+      do (format t "====================== ~%"))
+
+
+
